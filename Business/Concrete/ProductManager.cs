c8a86b57c48fc67763cbae1e55a0a3fab11987b5
yburak3 <yildirimburak@windowslive.com>
2022-09-15 15:30:15 +0300

    Cache, Performance, Transaction Aspects Added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -4,6 +4,7 @@ using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Performance;
 using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns;
